4a8d46c7366fd6c7f0cda2a8d6fc88e0a28c6245
aranggitoar <aranggi.josef@gmail.com>
2021-10-10 01:07:49 +0700

    Source refresh

--- bare-page.php
+++ bare-page.php
@@ -1,5 +1,6 @@
 <?php
 /**
+ * Template Name: Bare Page
  * The template for displaying all single posts
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
@@ -12,6 +13,7 @@ get_header();
 
 	<main id="primary" class="site-main">
 
+			get_template_part( 'template-parts/content', get_post_type() );
 
 	</main><!-- #main -->
 
