a7061406ee814497fbd792ef3b59422a22d22d69
aranggitoar <aranggi.josef@gmail.com>
2022-03-26 22:08:25 +0700

    ~

--- footer.php
+++ footer.php
@@ -15,22 +15,35 @@ if ( ! is_front_page() ) {
 		<div class="container">
 			<div id="about-us">
 				<div>
-					<h5>Tentang Kami</h5>
-					<p>Alkitab Kita adalah suatu pelayanan daring interdenominasional yang didukung dan diluncurkan oleh Yayasan Alkitab Bahasa Kita (Albata). Alkitab Kita hadir untuk memperlengkapi setiap anggota Tubuh Kristus memahami, menafsirkan dan mengajar Firman Tuhan di mana saja, kapan saja, bagaikan penerjemah Alkitab.</p>
-					<p>Kami berharap untuk melakukan itu lewat menyediakan: (1) materi berupa kursus dan artikel, (2) pendidikan Kristen yang alkitabiah, berkualitas, dan gratis, serta (3) akses kepada alat-alat penyelidikan Alkitab.</p> 
+          <svg width="48" height="48" version="1.1" viewBox="0 0 12.7 12.7" xmlns="http://www.w3.org/2000/svg">
+            <g transform="matrix(1.5 0 0 1.5 -8.3977e-7 .00032166)">
+              <g transform="translate(0,-11.25)" stroke-width=".008522">
+                <path d="m0.37703 13.483v4.0014h5.1531v-4.0014zm0.10995 0.12076h4.9332v3.7598h-4.9332z" fill="#fff6d5"/>
+                <path d="m2.7507 13.417v4.1322h5.3389v-4.1322zm0.17904 0.19664h4.9808v3.7389h-4.9808z" fill="#ffe680"/>
+              </g>
+            </g>
+          </svg>
+					<p>Alkitab Kita memperlengkapi Anda memahami Firman Allah — seperti penerjemah Alkitab.</p>
 				</div>
 
 				<div>
 					<h5>Tautan-tautan Penting</h5>
 					<ul class="footer-links">
-						<li><a href="https://alkitabkita.info">Beranda</a></li>
-						<li><a href="https://alkitabkita.info/tentang-kami">Tentang Kami</a></li>
 						<li><a href="https://bibledit.org:8083">Alkitab Studi</a></li>
 						<li><a href="https://alkitabkita.info/wiki/petunjuk-penggunaan-bibledit">Petunjuk Penggunaan Bibledit</a></li>
             <li><a href="<?php echo esc_attr( get_permalink( get_page_by_title( 'Kursus Alkitab Kita' ) ) ); ?>">Kursus Alkitab Kita</a></li>
 						<li><a href="<?php echo esc_attr( get_permalink( get_page_by_title( 'Artikel Alkitabiah' ) ) ); ?>">Artikel Alkitabiah</a></li>
 					</ul>
 				</div>
+        <div>
+          <div onclick="backToTop()" class="back-to-top">Kembali ke atas?</div>
+        </div>
+        <script>
+          function backToTop() {
+            document.body.scrollTop = 0;
+            document.documentElement.scrollTop = 0;
+          }
+        </script>
 			</div>
 		</div>
 		<div class="container">
