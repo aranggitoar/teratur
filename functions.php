f6669ac232e3711cfb0235334ca9a5812ae62a0e
aranggitoar <aranggi.josef@gmail.com>
2022-04-06 05:12:45 +0700

    Source refresh

--- functions.php
+++ functions.php
@@ -289,3 +289,39 @@ function remove_links_from_tutor_dashboard($links)
 	return $links;
 }
 add_filter('tutor_dashboard/nav_items', 'remove_links_from_tutor_dashboard');
+
+/**
+ * Page redirections.
+ */
+function page_redirection() {
+
+if ( isset( $_SERVER['HTTPS'] ) &&
+( $_SERVER['HTTPS'] == 'on' || $_SERVER['HTTPS'] == 1 ) ||
+isset( $_SERVER['HTTP_X_FORWARDED_PROTO'] ) &&
+$_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https' ) {
+  $protocol = 'https://';
+}
+else {
+  $protocol = 'http://';
+}
+
+$currenturl = $protocol . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+$currenturl_relative = wp_make_link_relative($currenturl);
+
+switch ( $currenturl_relative ) {
+
+  case '/dashboard':
+  $urlto = home_url('/dashboard/enrolled-courses');
+  break;
+
+  default:
+  return;
+
+}
+
+if ( $currenturl != $urlto )
+exit( wp_redirect( $urlto ) );
+
+
+}
+add_action( 'template_redirect', 'page_redirection' );
