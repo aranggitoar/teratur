43fee996682541e76c5811dfb24ca41c9711683a
aranggitoar <aranggi.josef@gmail.com>
2022-04-13 21:29:23 +0700

    Source refresh

--- js/course-landing-page.js
+++ js/course-landing-page.js
@@ -1,13 +1,60 @@
+const eaelLoginRegistrationWrapper =
+    document.querySelector('.eael-login-registration-wrapper'),
+  eaelLoginForm =
+    document.querySelector('#eael-login-form-wrapper'),
+  eaelRegisterForm =
+    document.querySelector('#eael-register-form-wrapper'),
+  eaelRegisterFormPasswordConfirmation =
+    document.querySelector('#form-field-confirm_pass'),
+  eaelRegisterFormSubmitButton =
+    document.querySelector('#eael-register-submit'),
+  switcherMarkup = `
+      <div id="login-registration-form-switcher-wrapper">
+        <span id="login-form-switcher" onclick="switchToLoginForm()">Masuk</span>
+        <span id="registration-form-switcher" onclick="switchToRegistrationForm()">Daftar</span>
+      </div>`;
+
+eaelLoginRegistrationWrapper.insertAdjacentHTML( 'afterbegin',
+  switcherMarkup );
+
+
+eaelRegisterFormPasswordConfirmation.placeholder = 'Konfirmasi Kata Sandi';
+eaelRegisterFormSubmitButton.value = 'Ya, daftarkan saya!';
+
+/*
+ * Switch to login type form.
+ */
 function switchToLoginForm() {
-  let loginForm = "<?php $this->print_login_form(); ?>";
-  let wrapper = document.querySelector(".login-registration-form-wrapper");
-  wrapper.write("");
-  wrapper.write(loginForm);
+  switchDisplayedForm('login');
 }
 
+/*
+ * Switch to registration type form.
+ */
 function switchToRegistrationForm() {
-  let registrationForm = "<?php $this->print_register_form(); ?>";
-  let wrapper = document.querySelector(".login-registration-form-wrapper");
-  wrapper.write("");
-  wrapper.write(registrationForm);
+  switchDisplayedForm('register');
+}
+
+/*
+ * Switch the form by changing its display state.
+ * The default state is registration form.
+ *
+ * @param formType
+ */
+function switchDisplayedForm(formType) {
+  const eaelLoginSwitcher =
+    document.querySelector('#login-form-switcher'),
+  eaelRegisterSwitcher =
+    document.querySelector('#registration-form-switcher');
+  if (formType === 'login') {
+    eaelRegisterForm.setAttribute('style', 'display: none !important;');
+    eaelLoginForm.setAttribute('style', 'display: block !important;');
+    eaelRegisterSwitcher.setAttribute('style', 'border-bottom: none !important; opacity: .6 !important;');
+    eaelLoginSwitcher.setAttribute('style', 'border-bottom: 1px solid black !important; opacity: 1 !important;');
+  } else if (formType === 'register') {
+    eaelLoginForm.setAttribute('style', 'display: none !important;');
+    eaelRegisterForm.setAttribute('style', 'display: block !important;');
+    eaelLoginSwitcher.setAttribute('style', 'border-bottom: none !important; opacity: .6 !important;');
+    eaelRegisterSwitcher.setAttribute('style', 'border-bottom: 1px solid black !important; opacity: 1 !important;');
+  }
 }
