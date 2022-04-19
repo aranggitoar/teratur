9fff29cf1897882b841717a599fa58a2e05d6975
aranggitoar <aranggi.josef@gmail.com>
2022-04-19 23:55:38 +0700

    Source refresh

---
+++ js/search-result-page.js
@@ -0,0 +1,9 @@
+const postSearchResultGridItem =
+  document.querySelectorAll("article.elementor-grid-item");
+
+postSearchResultGridItem.forEach(item => {
+  item.style.cursor = "pointer";
+  item.addEventListener('click', () => {
+    item.querySelector('.elementor-post__title > a').click();
+  })
+}
