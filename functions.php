45617522848b6a6ce7fb32e0346352eab177e541
aranggitoar <aranggi.josef@gmail.com>
2022-04-21 20:37:12 +0700

    Source refresh

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@
 
 if ( ! defined( 'TERATUR_VERSION' ) ) {
 	// Replace the version number of the theme on each release.
-	define( 'TERATUR_VERSION', '1.1.51' );
+	define( 'TERATUR_VERSION', '1.1.52' );
 }
 
 if ( ! function_exists( 'teratur_setup' ) ) :
